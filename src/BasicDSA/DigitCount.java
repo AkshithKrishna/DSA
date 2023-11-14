b4702ffabeb7ff8b02a13b275e8a854ae12a2842
AkshithKrishna <akshithkrishna47@gmail.com>
2023-11-14 08:16:48 -0600

    reverse of number,digit extraction

--- src/BasicDSA/DigitCount.java
+++ src/BasicDSA/DigitCount.java
@@ -4,14 +4,29 @@ public class DigitCount {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n = 99999;
+		int n = 12345678;
+		digitCountAndDigitExtract(n);
+		reverseNumber(1234);
+	}
+
+	public static void digitCountAndDigitExtract(int n) {
 		int rem, rev, count = 0;
 		while (n != 0) {
 			rem = n % 10;
+			System.out.println(rem);
 			count++;
 			n = n / 10;
 		}
 		System.out.println(count);
 	}
 
+	public static void reverseNumber(int n) {
+		int rem, rev = 0;
+		while (n != 0) {
+			rem = n % 10;
+			rev = rev * 10 + rem;
+			n = n / 10;
+		}
+		System.out.println(rev);
+	}
 }
